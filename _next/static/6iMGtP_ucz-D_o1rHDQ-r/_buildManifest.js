a477f8cb44e1ce37c4cbc79a21154d011c4f75f1
Anand <anandmurti221b.com>
2026-02-17 10:44:02 +0530

    Updates

--- _next/static/6iMGtP_ucz-D_o1rHDQ-r/_buildManifest.js
+++ _next/static/6iMGtP_ucz-D_o1rHDQ-r/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hut//_next/:path+",
+        "destination": "/hut/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
